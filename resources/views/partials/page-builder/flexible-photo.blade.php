0013c51e660efd002418ddd2aa0624c755f894b2
Patrick Kerby <patrick@kerbydesign.com>
2025-03-03 12:32:12 -0700

    update the fullwidth photo style

--- resources/views/partials/page-builder/flexible-photo.blade.php
+++ resources/views/partials/page-builder/flexible-photo.blade.php
@@ -2,7 +2,7 @@
   $border = get_sub_field('following-border');
 @endphp
 
-<section class="flexible-content {{ $border ? 'border-follow' : '' }}">
+<section class="flexible-content full-wdith-photo {{ $border ? 'border-follow' : '' }}">
   <div class="full-width">
     @if( get_row_layout() == 'flexible-photo' )
         <img src="@sub('full_width_photo')" />
