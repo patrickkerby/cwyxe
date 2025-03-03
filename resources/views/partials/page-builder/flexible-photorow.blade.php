1c2bd6ba6ea2aed54700ba4db87bc6da63cd1268
Patrick Kerby <patrick@kerbydesign.com>
2025-03-03 16:36:45 -0700

    add vertical padding to photo row

--- resources/views/partials/page-builder/flexible-photorow.blade.php
+++ resources/views/partials/page-builder/flexible-photorow.blade.php
@@ -1,4 +1,4 @@
-<section class="flexible-content">
+<section class="flexible-content @hassub('vertical_padding')extrapadding-@sub('vertical_padding') @endsub">
   <div class="photo-row @foreach(get_sub_field('photos') as $photo)@set($count, $loop->count)@endforeach count-{{ $count }}">
     @if( get_row_layout() == 'flexible-photorow' )
         @foreach(get_sub_field('photos') as $photo)
