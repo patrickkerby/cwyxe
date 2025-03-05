e378f07c77681573588af4948a7a2485db265a27
Patrick Kerby <patrick@kerbydesign.com>
2025-03-05 15:02:54 -0700

    page builder - mobile styles

--- resources/views/partials/cta-homepage.blade.php
+++ resources/views/partials/cta-homepage.blade.php
@@ -10,20 +10,20 @@
                     <div class="content">
                         <h3>{{ $cta['cta_title'] }}</h3>
                         <p>{{ $cta['cta_text'] }}</p>
+                        <a href="{{ $cta['cta_button_link']['url'] }}" class="button">{{ $cta['cta_button_link']['title'] }}</a>      
+                        @if($cta['related_post_label'])
+                            <div class="content related_post">
+                                <p>{{ $cta['related_post_label'] }}</p>
+                                @php
+                                    $post = $cta['related_post'];
+                                    $url = get_permalink( $post->ID );
+                                @endphp
+                                @if($post)
+                                    <a href="{{ $url }}" class="button">{{ $post->post_title }}</a>
+                                @endif
+                            </div>
+                        @endif
                     </div>
-                    <a href="{{ $cta['cta_button_link']['url'] }}" class="button">{{ $cta['cta_button_link']['title'] }}</a>      
-                    @if($cta['related_post_label'])
-                        <div class="content related_post">
-                            <p>{{ $cta['related_post_label'] }}</p>
-                            @php
-                                $post = $cta['related_post'];
-                                $url = get_permalink( $post->ID );
-                            @endphp
-                            @if($post)
-                                <a href="{{ $url }}" class="button">{{ $post->post_title }}</a>
-                            @endif
-                        </div>
-                    @endif
                     <img src="{{ $cta['cta_image']['url'] }}" alt="{{ $cta['cta_image']['alt'] }}">
                 </section>
             @endif
@@ -36,21 +36,21 @@
                     <div class="content">
                         <h3>{{ $cta['cta_title'] }}</h3>
                         <p>{{ $cta['cta_text'] }}</p>
+                        <a href="{{ $cta['cta_button_link']['url'] }}" class="button">{{ $cta['cta_button_link']['title'] }}</a>      
+                        <img src="{{ $cta['cta_image']['url'] }}" alt="{{ $cta['cta_image']['alt'] }}">
+                        @if($cta['related_post_label'])
+                            <div class="content related_post">
+                                <p>{{ $cta['related_post_label'] }}</p>
+                                @php
+                                    $post = $cta['related_post'];
+                                    $url = get_permalink( $post->ID );
+                                @endphp
+                                @if($post)
+                                    <a href="{{ $url }}" class="button">{{ $post->post_title }}</a>
+                                @endif
+                            </div>
+                        @endif
                     </div>
-                    <a href="{{ $cta['cta_button_link']['url'] }}" class="button">{{ $cta['cta_button_link']['title'] }}</a>      
-                    @if($cta['related_post_label'])
-                        <div class="content related_post">
-                            <p>{{ $cta['related_post_label'] }}</p>
-                            @php
-                                $post = $cta['related_post'];
-                                $url = get_permalink( $post->ID );
-                            @endphp
-                            @if($post)
-                                <a href="{{ $url }}" class="button">{{ $post->post_title }}</a>
-                            @endif
-                        </div>
-                    @endif
-                    <img src="{{ $cta['cta_image']['url'] }}" alt="{{ $cta['cta_image']['alt'] }}">
                 </section>
             @endif
         @endforeach
